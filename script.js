63577378b53ef6cc99a3ef91aaa614d17331ae5b
Byaombe <34215129+byaombe@users.noreply.github.com>
2024-09-07 19:40:19 -0500

    added a button with onclick feature, added a prompt and computer random choice. strored human answer in a p element

--- script.js
+++ script.js
@@ -7,4 +7,23 @@ make
 
 */
 
-console.log("hello there");
+/* get computer choice */
+function getComputerChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    console.log(choices[(Math.floor(Math.random() * choices.length))]);
+}
+getComputerChoice()
+
+/*Get human choice */
+function getHumanChoice() {
+let text;
+let person = prompt("Rock, Paper, or Scissors?");
+if (person == null || person == "") {
+    text = "Please type your choice";
+}else {
+text = person;
+}
+document.getElementById("textChoice").innerHTML = text;
+}
+
+getHumanChoice()
