d56120b1026c1c42676312ac12bb2118e5bc696e
Byaombe <34215129+byaombe@users.noreply.github.com>
2024-09-27 13:40:57 -0500

    created the variables humanScore and computerScore, created 2 more functions with the logic to play a single round

--- script.js
+++ script.js
@@ -8,43 +8,103 @@ make
 */
 
 /* get computer choice */
-function getComputerChoice() {
-    let choices = ["Rock", "Paper", "Scissors"];
-    console.log(choices[(Math.floor(Math.random() * choices.length))]);
-}
-getComputerChoice()
 
-/*Get human choice */
+function getComputerChoice() {
+    let computerChoice = ["rock", "paper", "scissors"];
+
+    computerChoice = computerChoice[(Math.floor(Math.random() * computerChoice.length))];
+
+    let text1 = "Computer entered " + computerChoice;
+
+    document.getElementById("cwordChoice").innerHTML = text1;
+
+    return computerChoice; //RETURN COMPUTER CHOICE
+}
+getComputerChoice();
+
+//Get human choice 
 function getHumanChoice() {
 
-   
-
-
 let text;
-let person = prompt("Rock, Paper, or Scissors?");
+let personChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
-if (person == "rock"|| person=="paper" || person == "scissors") {
-     text = "you have entered " + person;
-} else if ( person == null || person == ""){
+if (personChoice == "rock"|| personChoice=="paper" || personChoice== "scissors") {
+     text = "you have entered " + personChoice;
+} else if ( personChoice == null || personChoice == ""){
     text= "Please type your choice"
+    personChoice = null;
 } else {
     text = "That was an invalid choice"
+    personChoice = null;
 }
 document.getElementById("textChoice").innerHTML = text;
 
+ return personChoice;
 }
-getHumanChoice()
 
 
-/*
-keep track of the scores
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function playGame() {
+    //keep track of the scores
+
 let humanScore = 0;
 let computerScore = 0;
 
+
+for (let i = 0; i < 3; i++) {
+    playRound();
+}
+const personChoice = getHumanChoice(); //get human choice
+if (personChoice == null) {
+    return; //exit if input is invalid
+}
+const computerChoice = getComputerChoice(); //get computer choice
+
+//call plaRound with both choices
+playRound(personChoice, computerChoice)
 /*
 LOgic to play a single round
 */
-function playRound (humanChoice, ComputerChoice) {
+function playRound (personChoice, computerChoice) {
+    let announce;
+
+    if (personChoice === "rock" && computerChoice === "scissors" || 
+        personChoice === "paper" && computerChoice === "rock" || 
+        personChoice === "scissors" && computerChoice ==="paper") {
+        announce = "YOU WIN!";
+        humanScore++; //incriment human score
+
+    } else if (personChoice ===  computerChoice) {
+        announce = "IT'S A TIE!"
+    }else{
+    announce = "YOU LOSE, COMPUTER WINS";
+    computerScore++; //incriment computer score
+}
+   document.getElementById("winner").innerHTML = announce;
+}
+playRound()
 
 }
+playGame();
+
+
+
+
